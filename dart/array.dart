c6217a21ea75b958342101fe8cdcf2444285d281
rv-nataraj <rv.nataraj@gmail.com>
2021-09-17 10:46:08 +0000

    dart array

---
+++ dart/array.dart
@@ -0,0 +1,11 @@
+void main()
+{
+	var arr=['one','two','three','four','five'];
+	print(arr.first);
+	print(arr.last);
+	print(arr.length);
+	print(arr.isEmpty);
+	print(arr);
+	for(var i=0;i<arr.length;i++)
+		print(arr[i]);
+}
