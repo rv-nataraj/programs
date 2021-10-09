1afb25e0af83a6beed02ccb6e1639a4be34a4fcd
rv-nataraj <rv.nataraj@gmail.com>
2021-10-09 09:04:16 +0000

    update

--- let_us_start.sh
+++ let_us_start.sh
@@ -12,5 +12,8 @@ git config user.email kumar.me18@bsathy.ac.in
 git config user.name kumar-me18
 git config credential.helper store
 
+#link remote repository
+git remote add origin https://github.com/git-username/programs.git
+
 #creating git credentials (to avoid username password everytime)
 echo "https://rv-nataraj:personal_Access_token@github.com" > ~/.git-credentials 
