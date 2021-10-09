835c1aec6acfc5403cc0828d4e4228eaeabd5524
rv-nataraj <rv.nataraj@gmail.com>
2021-10-09 04:46:55 +0000

    letsstart

---
+++ let_us_start.sh
@@ -0,0 +1,8 @@
+mkdir ~/programs
+cd programs
+git init
+git config user.email kumar.me18@bsathy.ac.in
+git config user.name kumar-me18
+git config credential.helper store
+
+echo "https://rv-nataraj:personal_Access_token@github.com" >> ~/.git-credentials 
