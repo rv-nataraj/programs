09070c99beca28b03226dce1fe03b742c74a0b8f
rv-nataraj <rv.nataraj@gmail.com>
2021-10-09 05:59:45 +0000

    updated lus file

--- let_us_start.sh
+++ let_us_start.sh
@@ -1,8 +1,16 @@
+# creating a folder named programs in home directory
 mkdir ~/programs
+
+# change the working directory to programs
 cd programs
+
+#initializing git local repository
 git init
+
+#configuring git local repository
 git config user.email kumar.me18@bsathy.ac.in
 git config user.name kumar-me18
 git config credential.helper store
 
+#creating git credentials (to avoid username password everytime)
 echo "https://rv-nataraj:personal_Access_token@github.com" >> ~/.git-credentials 
