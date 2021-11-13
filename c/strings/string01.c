b8154b8f751cdd8e2130f84d20bcc33bccd1a540
rv-nataraj <rv.nataraj@gmail.com>
2021-11-13 04:44:56 +0000

    str01

---
+++ c/strings/string01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int age;
+    char name[20];
+    scanf("%d",&age);
+    scanf("%s",name);
+    printf("\nName %s",name);
+    printf("\nAge  %d",age);
+    return 0;
+}
