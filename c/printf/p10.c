3e723db43798b04301ff8884559a9d0b86eae329
rv-nataraj <rv.nataraj@gmail.com>
2021-12-07 07:10:47 +0000

    printf1

---
+++ c/printf/p10.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	short x;
+	printf("%d\n",sizeof(x));
+	return 0;
+}
+
---
+++ c/printf/p9.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	short x,y;
+	x = 32767;
+	printf("The value of x is %d %0x\n",x,x);
+	return 0;
+}
+
